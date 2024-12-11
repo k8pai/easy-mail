60297e36e7032ac38322326abd210218e283e2fe
k8pai <thek8pai@gmail.com>
2024-12-11 22:33:20 +0530

    refactor: removed unused comments

--- app/inbox/columns.tsx
+++ app/inbox/columns.tsx
@@ -6,60 +6,14 @@ import { DataTableColumnHeader } from '@/components/data-table-column-header'
 import { extractContentWithinBrackets } from '@/lib/helper'
 
 export const columns: ColumnDef<EmailsType>[] = [
-	// {
-	// 	id: 'select',
-	// 	header: ({ table }) => (
-	// 		<Checkbox
-	// 			checked={
-	// 				table.getIsAllPageRowsSelected() ||
-	// 				(table.getIsSomePageRowsSelected() && 'indeterminate')
-	// 			}
-	// 			onCheckedChange={(value) =>
-	// 				table.toggleAllPageRowsSelected(!!value)
-	// 			}
-	// 			aria-label="Select all"
-	// 			className="translate-y-[2px]"
-	// 		/>
-	// 	),
-	// 	cell: ({ row }) => (
-	// 		<Checkbox
-	// 			checked={row.getIsSelected()}
-	// 			onCheckedChange={(value) => row.toggleSelected(!!value)}
-	// 			aria-label="Select row"
-	// 			className="translate-y-[2px]"
-	// 		/>
-	// 	),
-	// 	enableSorting: false,
-	// 	enableHiding: false,
-	// },
-	// {`
-	// 	accessorKey: 'id',
-	// 	header: ({ column }) => (
-	// 		<DataTableColumnHeader column={column} title="Id" />
-	// 	),
-	// 	cell: ({ row }) => <div className="w-[80px]">{row.getValue('id')}</div>,
-	// 	enableSorting: false,
-	// 	enableHiding: false,
-	// },
 	{
 		accessorKey: 'Subject',
 		header: ({ column }) => (
 			<DataTableColumnHeader column={column} title="Subject" />
 		),
 		cell: ({ row }) => {
-			// const status = statuses.find(
-			// 	(status) => status.value === row.getValue('status')
-			// )
-
-			// if (!status) {
-			// 	return null
-			// }
-
 			return (
 				<div className="flex w-[500px] items-center">
-					{/* {status.icon && (
-						<status.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-					)} */}
 					<span>{row.getValue('Subject')}</span>
 				</div>
 			)
@@ -74,13 +28,9 @@ export const columns: ColumnDef<EmailsType>[] = [
 			<DataTableColumnHeader column={column} title="From" />
 		),
 		cell: ({ row }) => {
-			// const label = labels.find(
-			// 	(label) => label.value === row.original.label
-			// )
 			let from = extractContentWithinBrackets(row.getValue('From'))
 			return (
 				<div className="flex space-x-2">
-					{/* {label && <Badge variant="outline">{label.label}</Badge>} */}
 					<span className="max-w-[500px] truncate font-medium">
 						{from}
 					</span>
@@ -88,35 +38,4 @@ export const columns: ColumnDef<EmailsType>[] = [
 			)
 		},
 	},
-	// {
-	// 	accessorKey: 'priority',
-	// 	header: ({ column }) => (
-	// 		<DataTableColumnHeader column={column} title="Priority" />
-	// 	),
-	// 	cell: ({ row }) => {
-	// 		// const priority = priorities.find(
-	// 		// 	(priority) => priority.value === row.getValue('priority')
-	// 		// )
-
-	// 		// if (!priority) {
-	// 		// 	return null
-	// 		// }
-
-	// 		return (
-	// 			<div className="flex items-center">
-	// 				{priority.icon && (
-	// 					<priority.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-	// 				)}
-	// 				<span>{priority.label}</span>
-	// 			</div>
-	// 		)
-	// 	},
-	// 	filterFn: (row, id, value) => {
-	// 		return value.includes(row.getValue(id))
-	// 	},
-	// },
-	// {
-	// 	id: 'actions',
-	// 	cell: ({ row }) => <DataTableRowActions row={row} />,
-	// },
 ]
